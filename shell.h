09848bc7352ab53f4620b49bae030f216c3162a3
CodeSlayer123 <3613@holbertonschool.com>
2021-11-19 13:11:04 -0800

    first commit

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+int simple_shell(void);
+
+#endif
